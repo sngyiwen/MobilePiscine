c03e4c88134e3e607dc054f74a0f3572bc120189
jack <jack.sng@gmail.com>
2026-08-09 22:47:44 +0800

    ex01 completed

--- ex01/App.tsx
+++ ex01/App.tsx
@@ -1,14 +1,22 @@
-import { StatusBar } from "expo-status-bar";
+//// import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
 
 export default function App() {
+
+const[text, setText] = useState("A simple text");
+
   const handlePress = () => {
-    console.log("Button pressed");
+    if (text === "A simple text") {
+      setText("Hello World!");
+    } else {
+      setText("A simple text");
+    }
   };
 
   return (
     <View style={styles.container}>
-      <Text>A Simple text</Text>
+      <Text>{text}</Text>
       <Button title="Click me" onPress={handlePress} />
     </View>
   );
